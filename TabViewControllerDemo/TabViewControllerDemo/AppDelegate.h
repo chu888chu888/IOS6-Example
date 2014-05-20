ab91c44ca9ab49ed4c2a828d63522658a343fb27
chuguangming <chu888chu888@qq.com>
2014-05-20 13:32:33 +0800

    20140520

---
+++ TabViewControllerDemo/TabViewControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabViewControllerDemo
+//
+//  Created by chuguangming on 14-5-19.
+//  Copyright (c) 2014年 chuguangming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
